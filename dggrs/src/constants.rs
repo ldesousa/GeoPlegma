e8e3e18ef98a4f99b090d3e1e2263272edd0f8ad
Michael Jendryke <michael.jendryke@geoinsight.ai>
2025-08-19 10:51:10 +0200

    fixed Copyright notice

--- dggrs/src/constants.rs
+++ dggrs/src/constants.rs
@@ -1,3 +1,12 @@
+// Copyright 2025 contributors to the GeoPlegmata project.
+// Originally authored by Michael Jendryke, GeoInsight (michael.jendryke@geoinsight.ai)
+//
+// Licenced under the Apache Licence, Version 2.0 <LICENCE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENCE-MIT or http://opensource.org/licenses/MIT>, at your
+// discretion. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use geo::{Coord, Rect};
 
 pub fn whole_earth_bbox() -> Rect<f64> {
